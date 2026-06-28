807f28196ec1b8eaad620f81ff445b73f1d644e1
Sarah Schulte <git@sarah.engineer>
2026-06-27 20:47:11 -0400

    idk

--- pages/deploy.sh
+++ pages/deploy.sh
@@ -3,12 +3,7 @@ if [[ -z "${SESSION[id]}" ]]; then
   return $(status_code 401)
 fi
 
-delayed() {
-  sudo -u rc git pull
-  sleep 1
-  systemctl restart plotter-web
-}
-
-0&>- delayed 1&>- 2&>- &
-
 echo "OH HELL YEA. ok one sec"
+sudo -u rc git pull
+sleep 1
+systemctl restart plotter-web
