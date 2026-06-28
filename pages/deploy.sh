16203f5bcf2ca5eb7830c61e464408b702c44be1
Sarah Schulte <git@sarah.engineer>
2026-06-27 20:32:11 -0400

    oh you have to push

--- pages/deploy.sh
+++ pages/deploy.sh
@@ -1,2 +1,15 @@
+# very secure
+if [[ -z "${SESSION[id]}" ]]; then
+  return $(status_code 401)
+fi
+
 git pull
-sudo systemctl restart plotter-web
+
+delayed() {
+  sleep 3
+  sudo systemctl restart plotter-web
+}
+
+0&>- delayed 1&>- 2&>- &
+
+echo "OH HELL YEA. ok one sec"
