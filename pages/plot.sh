205f3d231660da425f544626313f5b118c1b6eba
Sarah Schulte <git@sarah.engineer>
2026-07-30 23:45:50 -0400

    didnt work

--- pages/plot.sh
+++ pages/plot.sh
@@ -39,7 +39,7 @@ jq -r '.code' <<< "$REQUEST_BODY" \
   | sed 's/;/;\n/g' > data/current
 
 
-setsid ./plot_task.sh < data/current 1>&- 2>&- &
+./plot_task.sh < data/current 1>&- 2>&- &
 
 echo "$!" > data/pid
 
